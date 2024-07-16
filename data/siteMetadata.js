300c29de8a82d2d5785f3c9e16e3a9081c1985d5
Ryan Pate <ryantpate97@gmail.com>
2024-07-15 19:48:33 -0700

    fix: disable newletter form

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Pates Blog',
   author: 'Ryan Pate',
   headerTitle: 'Pates Blog',
-  description: 'Somewhere to put my thoughts.',
+  description: 'A place for me to write about things I find interesting. Usually this will be my projects or solutions to problems I have encountered.',
   language: 'en-us',
   theme: 'dark', // system, dark or light
   siteUrl: 'https://ryantpate.com',
@@ -44,11 +44,11 @@ const siteMetadata = {
     //   googleAnalyticsId: '', // e.g. G-XXXXXXX
     // },
   },
-  newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
-    // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
-  },
+  // newsletter: {
+  //   // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
+  //   // Please add your .env file and modify it according to your selection
+  //   provider: 'buttondown',
+  // },
   comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
